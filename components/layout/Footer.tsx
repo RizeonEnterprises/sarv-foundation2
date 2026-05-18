97c33637b83edc146ab45de614b56bbd85159cc3
RizeonEnterprises <rizeonenterprises@gmail.com>
2026-05-18 16:21:28 +0530

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -32,15 +32,36 @@ export default function Footer() {
             </p>
 
             <div className="flex gap-4 mt-6">
-              <div className="w-9 h-9 bg-[#f38b27] rounded-full flex items-center justify-center text-white hover:scale-110 transition">
+              {/* LinkedIn (Link aana baaki hai) */}
+              <a 
+                href="#" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="w-9 h-9 bg-[#f38b27] rounded-full flex items-center justify-center text-white hover:scale-110 transition"
+              >
                 <FaLinkedinIn />
-              </div>
-              <div className="w-9 h-9 bg-[#f38b27] rounded-full flex items-center justify-center text-white hover:scale-110 transition">
+              </a>
+            
+              {/* Instagram */}
+              <a 
+                href="https://www.instagram.com/teamsarv?igsh=b2xycmM5M3E4dTJn" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="w-9 h-9 bg-[#f38b27] rounded-full flex items-center justify-center text-white hover:scale-110 transition"
+              >
                 <FaInstagram />
-              </div>
-              <div className="w-9 h-9 bg-[#f38b27] rounded-full flex items-center justify-center text-white hover:scale-110 transition">
+              </a>
+            
+              {/* Facebook */}
+              <a 
+                href="https://www.facebook.com/people/Sarv-Foundation/61589716634279/" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="w-9 h-9 bg-[#f38b27] rounded-full flex items-center justify-center text-white hover:scale-110 transition"
+              >
                 <FaFacebookF />
-              </div>
+              </a>
+            </div>
             </div>
           </div>
 
