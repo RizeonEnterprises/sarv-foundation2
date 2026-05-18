0172e80d2029f2da10978ece1cb4f554789985cb
RizeonEnterprises <rizeonenterprises@gmail.com>
2026-05-18 16:29:18 +0530

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -43,24 +43,24 @@ export default function Footer() {
               </a>
             
               {/* Instagram */}
-              <a 
+              <Link> 
                 href="https://www.instagram.com/teamsarv?igsh=b2xycmM5M3E4dTJn" 
                 target="_blank" 
                 rel="noopener noreferrer" 
                 className="w-9 h-9 bg-[#f38b27] rounded-full flex items-center justify-center text-white hover:scale-110 transition"
               >
                 <FaInstagram />
-              </a>
+              </Link>
             
               {/* Facebook */}
-              <a 
+              <Link> 
                 href="https://www.facebook.com/people/Sarv-Foundation/61589716634279/" 
                 target="_blank" 
                 rel="noopener noreferrer" 
                 className="w-9 h-9 bg-[#f38b27] rounded-full flex items-center justify-center text-white hover:scale-110 transition"
               >
                 <FaFacebookF />
-              </a>
+              </Link>
             </div>
             </div>
           </div>
